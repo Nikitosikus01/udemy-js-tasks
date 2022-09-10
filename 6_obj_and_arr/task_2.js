c13eacc97449eb3197e6861e371ea7f5f2e0f165
Nikitosikus01 <Nikitosikus01@gmail.com>
2022-09-10 17:16:12 +0200

    obj and arr task 1

---
+++ 6_obj_and_arr/task_2.js
@@ -0,0 +1,72 @@
+"Use strict";
+
+// У вас есть список учеников, которые хотят поиграть в игру:
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+// Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. 
+// Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+// Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+// Пример:
+// sortStudentsByGroups(students)  =>
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi'
+// ]
+// Если убрать одно студента из списка, то результат будет:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: -'
+// ]
+// А если добавить одного, то:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi, Somebody'
+// ]
+// То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
+
+
+
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    const resultArrey = [];
+    const arrGroup1 = [];
+    const arrGroup2 = [];
+    const arrGroup3 = [];
+    let st = 'Оставшиеся студенты: ';
+
+    arr.sort();
+    // решить задачу через вложенный цикл
+    
+
+}
+
+
+
+console.log(sortStudentsByGroups(students));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
