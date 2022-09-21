6501569a5a2e74add82e0e6ec792c1b7979093b6
Nikitosikus01 <Nikitosikus01@gmail.com>
2022-09-21 16:08:49 +0200

    'commit'

--- some projects/calculator.js
+++ some projects/calculator.js
@@ -4,61 +4,98 @@ function calculate(st){
     let arrSt = st.split(' ');
     let arrSymbols = '()*/+-'.split('');
 
-    function newArr (key){
+    function newArr (arr, key){
         let result;
-        switch (arrSt[key]) {
+        switch (arr[key]) {
             case '*':
-                result = arrSt[+key-1] * arrSt[+key+1];
-                arrSt.splice(key - 1, 3, result);
-                return arrSt;
+                result = arr[+key-1] * arr[+key+1];
+                arr.splice(key - 1, 3, result);
+                return arr;
                 
             case '/':
-                result = arrSt[+key-1] / arrSt[+key+1];
-                arrSt.splice(key - 1, 3, result);    
-                return arrSt;
+                result = arr[+key-1] / arr[+key+1];
+                arr.splice(key - 1, 3, result);    
+                return arr;
 
             case '+':
-                result = +arrSt[+key-1] + (+arrSt[+key+1]);
-                arrSt.splice(key - 1, 3, result);    
-                return arrSt;
+                result = +arr[+key-1] + (+arr[+key+1]);
+                arr.splice(key - 1, 3, result);    
+                return arr;
 
             case '-':
-                result = +arrSt[+key-1] - +arrSt[+key+1];
-                arrSt.splice(key - 1, 3, result);    
-                return arrSt;
+                result = +arr[+key-1] - +arr[+key+1];
+                arr.splice(key - 1, 3, result);    
+                return arr;
         
             default:
-                return 'Error. Key are not *, /, +, -';
+                return 'Input error.';
         }
     }
+    
+    
 
-    function division_multiplication(){
-        for (let key = 0; key < arrSt.length; key++) {
-            if (arrSt[key] === "/" || arrSt[key] === "*") {
-                newArr(key);
+    function division_multiplication(arr1){
+        for (let key = 0; key < arr1.length; key++) {
+            if (arr1[key] === "/" || arr1[key] === "*") {
+                newArr(arr1, key);
                 key = 0;
             } 
         }
+        return arr1;
     }
 
-    function addition_subtraction(){
-        for (let key = 0; key < arrSt.length; key++) {
-            if (arrSt[key] === "+" || arrSt[key] === "-") {
-                newArr(key);
+    function addition_subtraction(arr1){
+        for (let key = 0; key < arr1.length; key++) {
+            if (arr1[key] === "+" || arr1[key] === "-") {
+                newArr(arr1, key);
                 key = 0;
             } 
-        }   
+        }
+        return arr1;
     }
 
-    division_multiplication();
-    addition_subtraction();
+
+    
+    for (let key = 0; key < arrSt.length; key++) {
+        if (arrSt[key] === "(") {
+            let bracket2 = 0;
+            const arrInBrackets = [];
+            for(let i = key, j = 0; i ++;){
+                if (arrSt[i] === ")") {
+                    bracket2 = i;
+                    break;
+                }
+                arrInBrackets[j] = arrSt[i];
+                j++;
+            }
+            
+            division_multiplication(arrInBrackets);
+            addition_subtraction(arrInBrackets);
+
+            console.log(
+                key,
+                bracket2,
+                arrInBrackets,
+                arrSt
+            );
+            arrSt.splice(key, bracket2 - 1, arrInBrackets);
+            console.log(arrSt);
+        } 
+    }
+       
+    division_multiplication(arrSt);
+    addition_subtraction(arrSt);
+
 
     return arrSt[0].toFixed(0);
     // alert(arrSt[0].toFixed(0));
 }
 
 console.log(
-calculate('21 * 4 / 42 - 12'), // -10
+// calculate('21 * 4 / 42 - 12'), // -10
+calculate('21 * ( 40 / 4 ) - 120'), // 90
+calculate('21 * 40 / ( 41 - 1 )'), // 21
+// calculate('21 * ( 40 / ( -4 ) ) - 120'), // -330
 calculate('16 / 8 * 7 - 2 * 4 + 12') // 18
 
 );
